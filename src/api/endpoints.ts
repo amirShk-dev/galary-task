3d7a55ef6187c16d79ddef356ca606b1df6de2da
amirShk-dev <119292610+amirShk-dev@users.noreply.github.com>
2023-03-27 16:03:35 +0530

    Add files via upload

---
+++ src/api/endpoints.ts
@@ -0,0 +1,11 @@
+export default {
+    serverBaseURL:"https://api.unsplash.com",
+    client_id:"&client_id=JxPEMQb1Ru9cKQM9VwsyVu6kJ3rJOPP931_DGAhn0HQ",
+    per_page:"/photos?page=",
+    api:{
+        photos:"/photos?page=",
+        search:"/search/photos?page=",
+        query:"&query=",
+        topics:"/topics/"
+    }
+}
